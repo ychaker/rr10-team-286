86a6dbc77555fb0048ee384887e7ab5670744e24
Youssef Chaker <ychaker@opensourceconnections.com>
2010-10-15 20:23:33 -0400

    initial application setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depricateme::Application.config.secret_token = '631047ce9db7264a93cef937ce9eb328e8c0662c739d24eb0a9e5397b29bf8cad447beada13a9175ab140d5086e06b09a24506469e1685ea7bf3dc51921f043a'
